d53478510d2bc047aca0a79c1beb8360bff0276a
Christian Ramelow <info@codekandis.net>
2019-08-11 21:50:31 +0200

    Adds the `Base64DecoderInterface`.

---
+++ src/Base64DecoderInterface.php
@@ -0,0 +1,27 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Base64Codec;
+
+/**
+ * Represents the interface of all Base64 decoders.
+ * @package codekandis/base64-codec
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface Base64DecoderInterface
+{
+	/**
+	 * Decodes a value from a Base64 string.
+	 * @param string $value The Base64 string to decode.
+	 * @return string The decoded value.
+	 * @throws Base64Exception The value is not Base64 formatted.
+	 * @throws Base64Exception The padding of the value is invalid.
+	 */
+	public function decode( string $value ): string;
+
+	/**
+	 * Decodes a value from an URI safe Base64 string.
+	 * @param string $value The URL safe Base64 string to decode.
+	 * @return string The decoded value.
+	 * @throws Base64Exception The value is not Base64 URI safe formatted.
+	 */
+	public function decodeUriSafe( string $value ): string;
+}
