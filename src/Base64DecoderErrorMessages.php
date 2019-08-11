c2dac8664cc1bc71543c0a75512d29790c18b7a6
Christian Ramelow <info@codekandis.net>
2019-08-11 21:30:01 +0200

    Adds the `Base64DecoderErrorMessages`.

---
+++ src/Base64DecoderErrorMessages.php
@@ -0,0 +1,28 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Base64Codec;
+
+/**
+ * Represents the error messages of the Base64 decoder.
+ * @package codekandis/base64-codec
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class Base64DecoderErrorMessages
+{
+	/**
+	 * The value is not Base64 formatted.
+	 * @var string
+	 */
+	public const BAD_BASE64_FORMAT = 'The value is not Base64 formatted.';
+
+	/**
+	 * The value is not Base64 URI safe formatted.
+	 * @var string
+	 */
+	public const BAD_BASE64_URI_SAFE_FORMAT = 'The value is not Base64 URI safe formatted.';
+
+	/**
+	 * The padding of the value is invalid.
+	 * @int string
+	 */
+	public const BAD_BASE64_PADDING = 'The padding of the value is invalid.';
+}
