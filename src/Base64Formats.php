f5050dc1a9b6f55ef0489c426a7906d196702c42
Christian Ramelow <info@codekandis.net>
2019-08-11 21:42:35 +0200

    Adds the `Base64Formats`.

---
+++ src/Base64Formats.php
@@ -0,0 +1,42 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Base64Codec;
+
+/**
+ * Represents the valid Base64 formats.
+ * @package codekandis/base64-codec
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class Base64Formats
+{
+	/**
+	 * Stores the regular expression of the Base64 standard format.
+	 * @var string
+	 */
+	public const STANDARD_FORMAT = '^[A-Za-z0-9/\+]+=*$';
+
+	/**
+	 * Stores the unique character subset of the Base64 URI standard format in relation to the Base64 URI safe unique character subset.
+	 * @var string
+	 */
+	public const STANDARD_UNIQUE_CHARACTER_SUBSET = [
+		'/',
+		'+',
+		'='
+	];
+
+	/**
+	 * Stores the regular expression of the Base64 URI safe format.
+	 * @var string
+	 */
+	public const URI_SAFE_FORMAT = '^[A-Za-z0-9_\-]+$';
+
+	/**
+	 * Stores the unique character subset of the Base64 URI safe format in relation to the Base64 standard unique character subset.
+	 * @var string
+	 */
+	public const URI_SAFE_UNIQUE_CHARACTER_SUBSET = [
+		'_',
+		'-',
+		''
+	];
+}
