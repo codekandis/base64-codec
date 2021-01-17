de74d158305f8a80a3b913f31671b2162182fa5f
Christian Ramelow <info@codekandis.net>
2021-01-18 00:06:15 +0100

    Updates the data provider `Base64FormatGuardsWithInvalidBase64EncodedValuesExpectedExceptionsErrorCodesAndErrorMessagesDataProvider`.

--- tests/DataProviders/UnitTests/Base64FormatGuardInterfaceTest/Base64FormatGuardsWithInvalidBase64EncodedValuesExpectedExceptionsErrorCodesAndErrorMessagesDataProvider.php
+++ tests/DataProviders/UnitTests/Base64FormatGuardInterfaceTest/Base64FormatGuardsWithInvalidBase64EncodedValuesExpectedExceptionsErrorCodesAndErrorMessagesDataProvider.php
@@ -6,6 +6,8 @@ use CodeKandis\Base64Codec\Base64DecoderErrorCodes;
 use CodeKandis\Base64Codec\Base64DecoderErrorMessages;
 use CodeKandis\Base64Codec\Base64Exception;
 use CodeKandis\Base64Codec\Base64FormatGuard;
+use CodeKandis\Base64Codec\InvalidBase64PaddingException;
+use CodeKandis\Base64Codec\ValueIsNotBase64FormattedException;
 
 /**
  * Represents a data provider providing initiated Base64 format guards with invalid Base64 encoded values, expected exceptions, error codes and error messages.
@@ -27,8 +29,22 @@ class Base64FormatGuardsWithInvalidBase64EncodedValuesExpectedExceptionsErrorCod
 				],
 				1 => [
 					'base64FormatGuard'      => new Base64FormatGuard(),
+					'invalidEncodedValue'    => 'OMOHw4I8w4c8w6_Cr8Oxw5c3OEI-w69Bw4vCoTRXw69jwqfDv1DDrljDuDRcw4rDocKhdDc_L1PCtcOawrLCt3h9MMKkwq_Dp8K7TQ',
+					'expectedExceptionClass' => ValueIsNotBase64FormattedException::class,
+					'expectedErrorCode'      => Base64DecoderErrorCodes::BAD_BASE64_FORMAT,
+					'expectedErrorMessage'   => Base64DecoderErrorMessages::BAD_BASE64_FORMAT
+				],
+				2 => [
+					'base64FormatGuard'      => new Base64FormatGuard(),
 					'invalidEncodedValue'    => 'OMOHw4I8w4c8w6/Cr8Oxw5c3OEI+w69Bw4vCoTRXw69jwqfDv1DDrljDuDRcw4rDocKhdDc/L1PCtcOawrLCt3h9MMKkwq/Dp8K7TQ===',
 					'expectedExceptionClass' => Base64Exception::class,
+					'expectedErrorCode'      => Base64DecoderErrorCodes::BAD_BASE64_PADDING,
+					'expectedErrorMessage'   => Base64DecoderErrorMessages::BAD_BASE64_PADDING
+				],
+				3 => [
+					'base64FormatGuard'      => new Base64FormatGuard(),
+					'invalidEncodedValue'    => 'OMOHw4I8w4c8w6/Cr8Oxw5c3OEI+w69Bw4vCoTRXw69jwqfDv1DDrljDuDRcw4rDocKhdDc/L1PCtcOawrLCt3h9MMKkwq/Dp8K7TQ===',
+					'expectedExceptionClass' => InvalidBase64PaddingException::class,
 					'expectedErrorCode'      => Base64DecoderErrorCodes::BAD_BASE64_PADDING,
 					'expectedErrorMessage'   => Base64DecoderErrorMessages::BAD_BASE64_PADDING
 				]
